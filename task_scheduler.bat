fad904f2440d8f4f2cbc817cd1060aba4e539eab
Sydreai69 <sydreai@hotmail.com>
2024-07-22 10:19:03 +0200

    Task Schedular push and jsonld_extract.py update

---
+++ task_scheduler.bat
@@ -0,0 +1,9 @@
+# On déplace les fichiers anciennement téléchargés
+move C:\\Users\\roude\\Documents\\itineraire\\data\\jsonld\\*.jsonld C:\\Users\\roude\\Documents\\itineraire\\data\\jsonld\\previous_versions
+# On récupère la date qui nous servira à renommer nos fichiers
+set mydate=%date:~6,4%%date:~3,2%%date:~0,2%
+# On récupère nos flux compréssés qu'on va décomprésser et sauvegarder dans des fichiers spécifiques portant la date pour le versioning
+curl --compressed https://diffuseur.datatourisme.fr/webservice/2d170ab3322c5d5ade969c185d7aa1f7/2197898d-3506-444f-bc6d-6789d6cf0888 --output C:\\Users\\roude\\Documents\\itineraire\\data\\jsonld\\lieux-%mydate%.jsonld
+curl --compressed https://diffuseur.datatourisme.fr/webservice/1db5576a244ab8adff34886e9c746172/2197898d-3506-444f-bc6d-6789d6cf0888 --output C:\\Users\\roude\\Documents\\itineraire\\data\\jsonld\\produits-%mydate%.jsonld
+curl --compressed https://diffuseur.datatourisme.fr/webservice/e647c3e07526fb416f16ee3fd25ce334/2197898d-3506-444f-bc6d-6789d6cf0888 --output C:\\Users\\roude\\Documents\\itineraire\\data\\jsonld\\tours-%mydate%.jsonld
+curl --compressed https://diffuseur.datatourisme.fr/webservice/5dc3fe2b1b00a009b9b2e34a85750bf4/2197898d-3506-444f-bc6d-6789d6cf0888 --output C:\\Users\\roude\\Documents\\itineraire\\data\\jsonld\\events-%mydate%.jsonld
