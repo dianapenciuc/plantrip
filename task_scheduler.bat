ac216f8a16481fd59a78e55584352648ecc0a18f
Sydreai69 <sydreai@hotmail.com>
2024-08-03 11:37:11 +0200

    Push code BDD
    Version final du task_scheduler.bat
    Docker-compose pour la partie création de la base de données

--- task_scheduler.bat
+++ task_scheduler.bat
@@ -1,15 +1,34 @@
-#On crée les dossiers nécessaires:
-mkdir .\\data\\jsonld\\
-mkdir .\\data\\csv\\raw_extract\\
-mkdir .\\data\\csv\\transformed\\
+### Etapes de préparation
+# On crée les dossiers nécessaires:
+mkdir .\\data\\jsonld\\previous_versions\\
+mkdir .\\data\\csv\\raw_extract\\previous_versions\\
+mkdir .\\data\\csv\\transformed\\previous_versions\\
+
+# On supprime les anciennes versions téléchargées
+del /Q .\\data\\jsonld\\previous_versions\\*
 
 # On déplace les fichiers anciennement téléchargés
 move .\\data\\jsonld\\*.jsonld .\\data\\jsonld\\previous_versions
 
+# On déplace les fichiers d'extraction s'ils existent déjà
+move .\\data\\csv\\raw_extract\\*.csv .\\data\\csv\\raw_extract\\previous_versions
+move .\\data\\csv\\transformed\\*.csv .\\data\\csv\\transformed\\previous_versions
+
+# On crée une sauvegarde de notre base de données présente dans le container itineraire/mysql-1
+docker exec mysql-1 mysqldump --default-character-set=utf8mb4 -uroot -prootadmin itineraire -r /projet/data/mysql_db/save_db.sql
+
+### Etapes de lancement / actualisation
 # On récupère la date qui nous servira à renommer nos fichiers
 set mydate=%date:~6,4%%date:~3,2%%date:~0,2%
+
 # On récupère nos flux compréssés qu'on va décomprésser et sauvegarder dans des fichiers spécifiques portant la date pour le versioning
-curl --compressed https://diffuseur.datatourisme.fr/webservice/2d170ab3322c5d5ade969c185d7aa1f7/2197898d-3506-444f-bc6d-6789d6cf0888 --output .\\data\\jsonld\\lieux-%mydate%.jsonld
+curl --compressed https://diffuseur.datatourisme.fr/webservice/c229282874eb94445dac9450649a6130/2197898d-3506-444f-bc6d-6789d6cf0888 --output .\\data\\jsonld\\lieux_1-%mydate%.jsonld
+curl --compressed https://diffuseur.datatourisme.fr/webservice/6ec7691f90a10a04e04732b250d35575/2197898d-3506-444f-bc6d-6789d6cf0888 --output .\\data\\jsonld\\lieux_2-%mydate%.jsonld
+curl --compressed https://diffuseur.datatourisme.fr/webservice/8435363fa282ec0c8bbf851335536325/2197898d-3506-444f-bc6d-6789d6cf0888 --output .\\data\\jsonld\\lieux_3-%mydate%.jsonld
+curl --compressed https://diffuseur.datatourisme.fr/webservice/340629804e573eb5fd00491062f7b2d6/2197898d-3506-444f-bc6d-6789d6cf0888 --output .\\data\\jsonld\\lieux_4-%mydate%.jsonld
 curl --compressed https://diffuseur.datatourisme.fr/webservice/1db5576a244ab8adff34886e9c746172/2197898d-3506-444f-bc6d-6789d6cf0888 --output .\\data\\jsonld\\produits-%mydate%.jsonld
 curl --compressed https://diffuseur.datatourisme.fr/webservice/e647c3e07526fb416f16ee3fd25ce334/2197898d-3506-444f-bc6d-6789d6cf0888 --output .\\data\\jsonld\\tours-%mydate%.jsonld
 curl --compressed https://diffuseur.datatourisme.fr/webservice/5dc3fe2b1b00a009b9b2e34a85750bf4/2197898d-3506-444f-bc6d-6789d6cf0888 --output .\\data\\jsonld\\events-%mydate%.jsonld
+
+# On lance le docker; cette étape, si déjà lancée une 1ère fois, relancera le container etl-1 pour l'extraction des nouvelles données, la création des fichiers csv et l'alimentation de la base de données mysql
+docker-compose up -d
