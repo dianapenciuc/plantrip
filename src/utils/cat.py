cd9f213921a3e4a85d5a6d94a3fec415432860b7
olaaumari <99741334+olaaumari@users.noreply.github.com>
2024-08-05 07:51:11 +0200

    Create cat.py

---
+++ src/utils/cat.py
@@ -0,0 +1,62 @@
+cat = {
+    "Lieu": [
+        "Commerce de détail",
+        "Site naturel",
+        "Site sportif, récréatif et de loisirs",
+        "Service d'information touristique",
+        "Hébergement",
+        "Site d'affaires",
+        "Prestataire d'activité",
+        "Lieu de santé",
+        "Fournisseur de dégustation",
+        "Service pratique",
+        "Transport",
+        "Site culturel",
+        "Restauration",
+        "Prestataire de service"
+    ],
+    "Itinéraire touristique": [
+        "Itinéraire sous-marin",
+        "Itinéraire fluvial ou maritime",
+        "Itinéraire cyclable",
+        "Itinéraire pédestre",
+        "Itinéraire routier",
+        "Itinéraire équestre"
+    ],
+    "Fête et manifestation": [
+        "Évènement sports et loisirs",
+        "Evènement professionnel d'entreprise",
+        "Evènement social",
+        "Évènement culturel",
+        "Evènement commercial"
+    ],
+    "Produit": [
+        "Visite",
+        "Produit d'hébergement",
+        "Pratique",
+        "Location"
+    ]
+}
+
+
+
+
+poi = {
+    'Monuments': ["Sainte-Chapelle", "Théâtre du Châtelet", "Tour Eiffel", "Opéra Garnier", 
+    "Panthéon", "Basilique Saint-Denis", "Arc de Triomphe", "Cathédrale Notre-Dame"],
+    'Quartiers & lieux emblématiques': ["La Défense", "Quartier Montmartre", "Saint-Germain-des-Prés", 
+    "Gare du Nord", "Champs-Élysées", "Cimetière Père Lachaise", "Cimetière du Montparnasse"],
+    'Musées': ["Musée d'Orsay", "Musée du Chocolat", "Espace Dali", "Musée de Cluny - Musée national du Moyen Âge", 
+    "Musée de la Chasse et de la Nature", "Fondation Louis Vuitton", "Musée de l'Air et de l'Espace", 
+    "Musée de la Musique", "Musée de l'Orangerie", "Musée du Louvre", 
+    "Musée national des arts asiatiques-Guimet", "Musée de l'illusion", "Musée de la Musique", 
+    "Musée national de la Marine de Paris", "Musée Picasso", "Muséum national d'Histoire naturelle", 
+    "Palais de la Découverte", "Palais Royal", "Bourse de commerce", "Cité des Sciences et de l'Industrie", 
+    "Musée des Arts et Métiers", "Musée Grévin", "Musée de l'Armée"],
+    'Châteaux': ["Château de Vincennes", "Château de Rambouillet", "La Malmaison", "Chateau de Versailles", 
+    "Domaine de Chantilly", "Château de Fontainebleau", "Château de Vaux-le-Vicomte"],
+    'Parcs & jardins': ["Parc de la Villette", "Parc des Buttes-Chaumont", "Parc de Sceaux", 
+    "Parc Floral de Paris", "Jardin des Tuileries", "Parc de la Butte-du-Chapeau-Rouge", 
+    "Jardin des Plantes", "Parc de Bagatelle"],
+    'Divertissement et loisirs': ["Parc des Princes", "Parc Zoologique", "Disneyland Paris"]
+}
